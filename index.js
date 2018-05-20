4e2ad138fe689243780679eab8090f47a53535b0
fedea <fedeaperez@hotmail.com.ar>
2018-05-20 20:31:35 -0300

    all working

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var _BusinessAPI = require('./src/BusinessAPI');
 var _GUIDAPI = require('./src/GUIDAPI');
 var _TrackAPI = require('./src/TrackAPI');
 var _IntentAPI = require('./src/IntentAPI');
+var _MessagesAPI =require('./src/MessagesAPI');
 
 exports.MessagesContentAPI = _MessagesContentAPI;
 exports.ProspectsAPI = _ProspectsAPI;
@@ -16,3 +17,4 @@ exports.BusinessAPI = _BusinessAPI;
 exports.GUIDAPI = _GUIDAPI;
 exports.TrackAPI = _TrackAPI;
 exports.IntentAPI = _IntentAPI;
+exports.MessagesAPI = _MessagesAPI;
