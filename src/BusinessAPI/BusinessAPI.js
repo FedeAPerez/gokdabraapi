457b7152f58eab98829c33261cd82e3f147f746a
fedea <fedeaperez@hotmail.com.ar>
2018-05-20 20:15:28 -0300

    all ok

--- src/BusinessAPI/BusinessAPI.js
+++ src/BusinessAPI/BusinessAPI.js
@@ -1,21 +1,24 @@
 'use strict';
 var  BusinessAPI = function() {
-    var business = [
+    var _getBusiness = function() {
+      var business = [
       { number: 1, business_name: "Doers", },
       { number: 2, business_name: "KDABRA" },
       { number: 3, business_name: "eColitas" },
       { number: 4, business_name: "fotoflasheventos" },
       { number: 5, business_name: "dexter" },
       { number: 6, business_name: "prune" }
-    ]
+      ]
+      return business;
+    }
 
     var all = function() {
-      return business;
+      return _getBusiness;
     }
 
     var getBusinessByName = function(name) {
       const isBusiness = p => p.business_name.toLowerCase() === name.toLowerCase();
-      return business.find(isBusiness);
+      return _getBusiness().find(isBusiness);
     }
 
     return {
